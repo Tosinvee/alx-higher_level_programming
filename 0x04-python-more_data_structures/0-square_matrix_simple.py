7af73ae3780614cff48994fe829aa2ad821c13f6
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-01-23 13:21:45 -0800

    alx

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), elm)) for elm in matrix]
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
