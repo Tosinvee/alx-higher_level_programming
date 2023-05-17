53efe54bcb3c11c7fa439b770c3c5ffb3d42e68a
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-05-16 17:04:46 -0700

    insert

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a table called first_table with values id and name.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- full description of the table
+SHOW CREATE TABLE first_table;
