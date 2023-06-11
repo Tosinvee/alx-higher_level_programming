8fc64de71e470c6c6652dfdc69693fe5a620c206
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-11 05:45:40 -0700

    comment

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+--  a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
 	id INT AUTO_INCREMENT NOT NULL,
