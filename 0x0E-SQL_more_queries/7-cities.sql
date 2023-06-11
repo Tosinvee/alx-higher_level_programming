a2364ba4b95ece6b6aa1b070b5a45a75cb8c2bb3
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-11 06:16:26 -0700

    correction

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,9 @@
 --  a script that creates the database hbtn_0d_usa and the table cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE NOT NULL AUTO INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY('id'),
-	FOREIGN KEY('id'));
+	PRIMARY KEY(id),
+	FOREIGN KEY(state_id)
+	REFERENCES hbtn_0d_usa.state(id));
