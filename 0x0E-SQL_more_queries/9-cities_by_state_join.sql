57c3946b4763fd91a6000d07213892118ed0f9c6
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-11 08:15:14 -0700

    inner_join

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- a script that lists all cities contained in the database hbtn_0d_us
+-- list all rows that contains in a database
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = state.id
+ORDER BY cities.id
