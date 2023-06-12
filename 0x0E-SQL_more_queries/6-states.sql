eb8c11b67dc6babd2ab488922f397761d13f325d
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-11 22:20:07 -0700

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 --  a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-	id INT AUTO_INCREMENT NOT NULL,
+-- use a database
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY('id'));
+	PRIMARY KEY('id')); 
 
