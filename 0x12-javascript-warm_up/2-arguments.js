cae037671ec2e7c26903b7f6f4ceb0a73c4265f2
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-14 02:46:40 -0700

    executable

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const count = process.argv.lenght;
+if (count === 2) {
+  console.log('No argument');
+} else if (count === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
