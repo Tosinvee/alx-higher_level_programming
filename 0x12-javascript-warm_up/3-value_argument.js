f953573ec968feb0afb03d4477919daa2501b6c3
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-14 05:30:53 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 if (process.argv[2] !== undefined) {
   const firstArgument = process.argv[2];
-  console.log('First Argument:' + firstArgument);
+  console.log('First argument:' + firstArgument);
 } else {
   console.log('No argument');
 }
