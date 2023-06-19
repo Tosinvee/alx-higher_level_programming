0a2515e60d94a78dc8306c04137668f988bec195
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-18 20:00:47 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-lists all states from the database where name matches the argument
-
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import sys
 import MySQLdb
 if __name__ == "__main__":
