139281fd2764df5f8d800cc0157cdc059637ba4f
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-19 07:32:33 -0700

    .

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,6 +8,9 @@
                                     <database name>
                                     <state name searched>
 """
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
