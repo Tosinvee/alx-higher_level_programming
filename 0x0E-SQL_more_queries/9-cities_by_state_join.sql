0d6b580d7857e9be167f0940e5f2a0bec208ed78
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-23 09:30:58 -0700

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 -- a script that lists all cities contained in the database hbtn_0d_us
--- list all rows that contains in a database
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states
-ON cities.state_id = state.id;
+ON cities.state_id = states.id;
