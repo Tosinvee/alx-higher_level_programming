5451aad0b25dd13731cbe20522bfc1f2443e1b73
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-24 05:48:07 -0700

    correction

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    row = session.query(State).filter(State.name == (sys.argv[4],))
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
     try:
         print(instance[0].id)
     except IndexError:
