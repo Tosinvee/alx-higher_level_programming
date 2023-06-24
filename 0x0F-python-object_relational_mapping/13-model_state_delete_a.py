aa5584dafc56c8fdb55d0cd84f80ccc751f0a795
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-24 07:39:02 -0700

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 '''
-a script that deletes all State objects with a name containing the 
+a script that deletes all State objects with a name containing the
 letter a from the database
 '''
 
