0d035594a40c03e498bc5fe38ea0e74f95177f69
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-06-24 17:58:17 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 '''
-similar to model_state.py named model_city.py that contains the class definition of a City
+similar to model_state.py named model_city.py that contains
+the class definition of a City
 '''
 
 from model_state import Base
